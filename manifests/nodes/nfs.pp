68dac367615d3e59de5aa8ffa98cd559256edb2c
ralcus <ralcus.com@gmail.com>
2015-02-28 00:52:06 +0000

    Removed file setting up nfs out of the machine into the host as we are using the vagrant synced folder feature now this is not needed.

--- manifests/nodes/nfs.pp
+++ manifests/nodes/nfs.pp
@@ -3,14 +3,6 @@ package { 'nfs-kernel-server':
     ensure => installed
 }
 
-file { '/etc/exports':
-    ensure => present,
-    owner => root, group => root,
-    content => template('/vagrant/manifests/nodes/templates/nfs/exports.erb'),
-    notify => Service['nfs-kernel-server'], # ssh will restart whenever you edit this file.
-    require => Package['nfs-kernel-server']
-}
-
 service { 'nfs-kernel-server':
     ensure => running,
     enable => true,
