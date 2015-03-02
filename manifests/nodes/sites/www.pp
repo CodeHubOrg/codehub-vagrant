6a9952a5358409355758d0ce436b3b32d4c36899
ralcus <ralcus.com@gmail.com>
2015-03-02 00:37:56 +0000

    No need for a vhost for the www folder.

--- manifests/nodes/sites/www.pp
+++ manifests/nodes/sites/www.pp
@@ -1,20 +1,6 @@
-class www_folder {
-    $folder = ""
-    $docroot = $folder
-    $domain = "vagrant.dev.com"
-
-    file { "/var/www" :
-        ensure => directory,
-        group => "vagrant",
-        owner => "vagrant",
-        recurse => false,
-    }
-
-    apache::vhost { "${domain}":
-        port    => '80',
-        docroot => '/var/www',
-        template => "/vagrant/manifests/nodes/templates/vhosts/vhost.erb"
-    }
+file { "/var/www" :
+    ensure => directory,
+    group => "vagrant",
+    owner => "vagrant",
+    recurse => false,
 }
-
-class { 'www_folder': }
